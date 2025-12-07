ff2da5ff9321fcb36ad3ff5fe34f23f36999d13c
GayathriganeshNsrit <gayathrivemuri98@gmail.ccm>
2025-12-07 11:26:39 +0530

    Add BrowserRouter and update app structure

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import ClassComponent from './ClassComponent/classcomponent.jsx';
 import Counter1 from './Counterfiles/counterfiles.jsx';
 function App() {
   return (
-    <div>
-     <h1>This is app.js file</h1>
-     <FunctionComponent/>
-     <ClassComponent/>
-     <Counter1/>
-    </div>
+   <></> // An anomoyous tag or home tag
   );
 }
 
