bc6f74b7687459700169a81f933f147e402b2e3e
GayathriganeshNsrit <gayathrivemuri98@gmail.ccm>
2025-12-13 19:35:13 +0530

    I have modified the app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-import FunctionComponent from './FunctionalComponent/functionalcomponent.jsx'
-import ClassComponent from './ClassComponent/classcomponent.jsx'; 
-import Counter1 from './Counterfiles/counterfiles.jsx';
+import CounterFiles from './Counterfiles/counterfiles.jsx';
+import {Route, Routes} from 'react-router-dom';
+import FunctionComponent from './FunctionalComponent/functionalcomponent.jsx';
+import ClassComponent from './ClassComponent/classcomponent.jsx';
 function App() {
   return (
-   <></> // An anomoyous tag or home tag
+   <>
+  <Routes> {/* Routes is a container component which holds all the individual route definitions */}
+    <Route path='/' element={<CounterFiles/>}/> 
+    {/*Route is a self closing tag where we give the paths using this tag. 
+    Core Component that defines a specific path and the react element to render when that path matches 
+    / is an empty path. element contains the component that we need to pass. Here I want to add the Counter component
+    so i have given the Counter which is given as reference.
+    Whenever we open the page, the default page is opened.  
+    */}
+    <Route path='/classcomponent' element={<ClassComponent/>}/>
+    <Route path='/functioncomponent' element={<FunctionComponent/>}/>
+    </Routes>
+   </> // An anomoyous tag or home tag
   );
 }
 
