94fe990a20f425a1c13a2754192f2a817c63bd02
Gayathri <58878183+GayathriganeshNsrit@users.noreply.github.com>
2025-12-13 19:54:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+/*Route is a self closing tag where we give the paths using this tag. 
+    Core Component that defines a specific path and the react element to render when that path matches 
+    / is an empty path. element contains the component that we need to pass. Here I want to add the Counter component
+    so i have given the Counter which is given as reference.
+    Whenever we open the page, the default page is opened.  
+    */
 import './App.css';
 import CounterFiles from './Counterfiles/counterfiles.jsx';
 import {Route, Routes} from 'react-router-dom';
@@ -8,12 +14,6 @@ function App() {
    <>
   <Routes> {/* Routes is a container component which holds all the individual route definitions */}
     <Route path='/' element={<CounterFiles/>}/> 
-    {/*Route is a self closing tag where we give the paths using this tag. 
-    Core Component that defines a specific path and the react element to render when that path matches 
-    / is an empty path. element contains the component that we need to pass. Here I want to add the Counter component
-    so i have given the Counter which is given as reference.
-    Whenever we open the page, the default page is opened.  
-    */}
     <Route path='/classcomponent' element={<ClassComponent/>}/>
     <Route path='/functioncomponent' element={<FunctionComponent/>}/>
     </Routes>
